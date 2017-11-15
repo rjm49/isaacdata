dc5ea85ee311403c3858e9e17627e62a12ac6d6c
Russell Moore <rjm49@cl.cam.ac.uk>
2017-11-15 22:35:12 +0000

    MCMC now with Fail matrix

--- mcmc/MCMC.py
+++ mcmc/MCMC.py
@@ -8,13 +8,16 @@ from utils.utils import extract_runs_w_timestamp
 
 if __name__ == '__main__':
     n_users = -1
-    n_steps = 100000
+    n_steps = 100e6
     cats, cat_lookup, all_qids, users, _stretches_, levels, cat_ixs = init_objects(n_users, seed=666)
     passdiffs, stretches, passquals, all_qids = load_new_diffs()
-    n_users = len(users)
 
+    users = numpy.unique(users)
+    n_users = len(users)
+    print("kickoff for n_users?",n_users)
 
     observed_qids = []
+    failed_qids = []
 
     usersf = open("mcmc_users.txt","w")
 
@@ -26,16 +29,28 @@ if __name__ == '__main__':
         for run in runs:
             ts, q, n_atts, n_pass = run
             q = q.replace("|","~")
-            if n_pass>0 and  q not in observed_qids:
-                observed_qids.append(q)
+            if n_pass>0:
+                if q not in observed_qids:
+                    observed_qids.append(q)
+            else:
+                if q not in failed_qids:
+                    failed_qids.append(q)
+
 
     usersf.close()
     òbserved_qids = ["ROOT"] + observed_qids
     with open("obsqs.txt","w") as qf:
         qf.write("\n".join(observed_qids))
 
+    with open("failqs.txt","w") as ff:
+        ff.write("\n".join(failed_qids))
+
     n_qids = len(observed_qids)
+    n_fqids = len(failed_qids)
     X = numpy.zeros(shape=(n_qids, n_qids))  # init'se a new feature vector w same width as all_X
+
+    #F=None
+    F = numpy.zeros(shape=(n_qids, n_fqids)) #failure matrix
 
     for u in users:
         print("user = ", u)
@@ -49,7 +64,10 @@ if __name__ == '__main__':
                 qix = observed_qids.index(q)
                 X[curr_qix, qix] = X[curr_qix, qix]+1
                 curr_qix = qix
-
+            else: #register failed attempts here
+                if F is not None:
+                    qix = failed_qids.index(q)
+                    F[curr_qix, qix] = F[curr_qix, qix]+1
 
 
     #Numpy divide each row in X by its sum to normalise to probabilties
@@ -58,8 +76,12 @@ if __name__ == '__main__':
     print(X.shape)
     X = X / X.sum(axis=1, keepdims=True)
     X = numpy.nan_to_num(X)
-
     numpy.savetxt("X.csv", X, delimiter=",")
+
+    if F is not None:
+        #F = F/F.sum(axis=1, keepdims=True)
+        F = numpy.nan_to_num(F)
+        numpy.savetxt("F.csv", F, delimiter=",")
 
     q_cnt = numpy.zeros(shape=n_qids)
     i=0
