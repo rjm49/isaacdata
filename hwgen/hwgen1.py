f9d3f9f2321ed1d276c52426d5376c83df22804e
rjm49 <Kr4k4to4!>
2018-02-13 15:07:23 +0000

    combined hw gen script

--- hwgen/hwgen1.py
+++ hwgen/hwgen1.py
@@ -1,4 +1,8 @@
+from collections import Counter
+from datetime import timedelta, datetime, MINYEAR
+
 import pandas as pd
+import random
 import json
 
 from backfit.BackfitUtils import init_objects
@@ -6,42 +10,77 @@ from backfit.BackfitUtils import init_objects
 base = "../../../isaac_data_files/"
 df = pd.read_csv(base+"hwgen1.csv", index_col=0, header=0)
 
+base = "../../../isaac_data_files/"
+#choose a teacher for whose class to create homework
+teacher_df = pd.read_csv(base+"groups.csv")
+groupmem_df = pd.read_csv(base+"group_memberships.csv")
+
 idx = df.index
-concepts = set()
-for qid in idx:
-    rawval = df.loc[qid, "related_concepts"]
-    if not pd.isna(rawval):
-        rcs = eval(rawval)
-        concepts.update(rcs)
+# concepts = set()
+# for qid in idx:
+#     rawval = df.loc[qid, "related_concepts"]
+#     if not pd.isna(rawval):
+#         rcs = eval(rawval)
+#         concepts.update(rcs)
 
-for c in sorted(concepts):
-    print(c)
-print(len(concepts))
+# for c in sorted(concepts):
+#     print(c)
+# print(len(concepts))
 
-n_
+n_users = 1000
 cats, cat_lookup, all_qids, users, _stretches_, levels, cat_ixs = init_objects(n_users,
                                                                                path="../../../isaac_data_files/",
                                                                                seed=666)
 
-runs = open(base+"by_runs/{}.txt".format(u)).readlines()
-        u_run_ct = len(runs)
-        all_zero_level = True
-        for run_ix,run in enumerate(runs):
-            run_ct+=1
+run_ct = 0
+max_ts = datetime(1,1,MINYEAR)
+print("Generating files for {} users...".format(len(users)))
+
+
+while True:
+    r = random.randint(0, teacher_df.shape[0])
+    group = teacher_df.iloc[r, :]
+    t_id = group["owner_id"]
+    g_id = group["id"]
+    members = groupmem_df[groupmem_df["group_id"]==g_id]
+    users = members["user_id"]
+    print(g_id, t_id, list(users))
+
+    for u in users:
+        try:
+            runs = open("../../../isaac_data_files/by_runs/{}.txt".format(u)).readlines()
+        except FileNotFoundError:
+            continue
+        for run_ix, run in enumerate(runs):
             ts, q, n_atts, n_pass = eval(run)
-            if u_start_ts is None:
-                u_start_ts = pd.to_datetime(ts)
-            u_end_ts = pd.to_datetime(ts)
-            #fout.write(",".join(map(str,(run))) + "\n")
-            # fout.write(str(run)+"\n")
-            # continue
-            qt = q.replace("|","~")
-            lev = levels[qt]
-            pass
-            if(lev!=0):
-                all_zero_level=False
-            else:
-                pass
-            print(all_types)
-            # atix = all_types.index( str( atypes.loc[qt,7] ) )
-            # print(atix)
+            if pd.to_datetime(ts) > max_ts:
+                max_ts = pd.to_datetime(ts)
+
+    ts_cutoff = max_ts - timedelta(days=7)
+
+    all_concepts = Counter()
+    all_cats = Counter()
+    for u in users:
+        try:
+            runs = open("../../../isaac_data_files/by_runs/{}.txt".format(u)).readlines()
+        except FileNotFoundError:
+            continue
+        for run_ix, run in enumerate(runs):
+            run_ct += 1
+            ts, q, n_atts, n_pass = eval(run)
+            if(pd.to_datetime(ts) < ts_cutoff or q not in df.index):
+                continue
+
+            cat = cat_lookup[q.replace("|","~")]
+            concepts_raw = df.loc[q, "related_concepts"]
+            concepts = eval(df.loc[q, "related_concepts"]) if not pd.isna(concepts_raw) else []
+            #print(ts, u, q, concepts)
+            all_cats[cat]+=1
+            all_concepts.update(concepts)
+
+    print(ts_cutoff)
+    print(all_cats.most_common(10))
+    print(all_concepts.most_common(10))
+    print(len(all_concepts))
+
+    input("prompt")
